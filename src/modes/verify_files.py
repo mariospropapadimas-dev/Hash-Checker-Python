c29480b0b339be8d081b2ceb3342edcf0425ab9b
mariospapadimas <mariospropapadimas@gmail.com>
2026-09-11 15:55:47 +0300

    Working on file uploading

--- src/modes/verify_files.py
+++ src/modes/verify_files.py
@@ -1,6 +1,7 @@
 """The VerifyFilesMode mode for the application."""
 
 import tkinter as tk
+from tkinter import filedialog
 
 
 class VerifyFilesMode:
@@ -9,15 +10,30 @@ class VerifyFilesMode:
 
     def __init__(self, parent, design):
         """Initialize the VerifyFilesMode mode."""
+        self.file_path = None
         self.parent = parent
         self.design = design
 
-        self.under_construction = tk.Label(
+        self.select_file = tk.Button(
             self.parent,
-            text="Under Construction",
+            text="Select File",
+            font=self.design.main_text_font,
+            command=self.select_file
+        )
+
+        self.selected_file_path = tk.Label(
+            self.parent,
+            text= f"Select File: {self.file_path}",
             font=self.design.main_text_font
         )
 
+
     def draw_ui(self):
         """Draw the UI."""
-        self.under_construction.pack()
+        self.select_file.pack(ipady=10)
+        self.selected_file_path.pack(ipady=1)
+
+    def select_file(self):
+        """Select the file to verify and print path"""
+        self.file_path = filedialog.askopenfilename()
+        print(self.file_path)
